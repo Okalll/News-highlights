d151fe15039be0be0dc4bde104a0217f5cfb5370
Okall Vivian <vivieokall@gmail.com>
2019-02-04 15:53:50 +0300

    "Update request.py"

--- app/request.py
+++ app/request.py
@@ -1,33 +1,35 @@
 from app import app
-import urllib.request,json
+import urllib.request
+import json
 from .models import article
 
-Article = article.article
+Article = article.Article
 
 # Getting api key
-api_key = app.config['MOVIE_API_KEY']
+api_key = app.config['ARTICLE_API_KEY']
 
-#Getting the article base url
+# Getting the article base url
 base_url = app.config["ARTICLE_API_BASE_URL"]
+
 
 def get_articles(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_articles_url = base_url.format(category,api_key)
+    get_articles_url = base_url.format(category, api_key)
 
     with urllib.request.urlopen(get_articles_url) as url:
         get_articles_data = url.read()
         get_articles_response = json.loads(get_articles_data)
 
-        article_results = None
+        article_articles = None
 
-        if get_articles_response['results']:
-            article_results_list = get_articles_response['results']
-            article_results = process_results(article_results_list)
+        if get_articles_response['articles']:
+            article_articles_list = get_articles_response['articles']
+            article_articles = process_results(article_articles_list)
 
+    return article_articles
 
-    return ]article_results
 
 def process_results(article_list):
     '''
@@ -46,4 +48,4 @@ def process_results(article_list):
         title = article_item.get('title')
         publishedAt = article_item.get('time published')
 
-     return article_results
+    return article_articles
